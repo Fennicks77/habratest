b6c4a5da386bd5f199969e907b8f40e75dbb7af9
Nick <nituev7@gmail.com>
2015-02-26 19:47:53 +0300

    Фича с ЯндексБЭМ

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,343 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "sssr"
+    },
+    {
+        "name": "sssr",
+        "elems": [
+            {
+                "name": "header"
+            }
+        ]
+    },
+    {
+        "name": "sssr",
+        "elems": [
+            {
+                "name": "logo"
+            }
+        ]
+    },
+    {
+        "name": "icon"
+    },
+    {
+        "name": "icon",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "sssr"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "form"
+    },
+    {
+        "name": "form",
+        "elems": [
+            {
+                "name": "search"
+            }
+        ]
+    },
+    {
+        "name": "input"
+    },
+    {
+        "name": "input",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "islands"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "input",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "m"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "input",
+        "mods": [
+            {
+                "name": "has-clear",
+                "vals": [
+                    {
+                        "name": true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button"
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "islands"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "m"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "button",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "submit"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "spin"
+    },
+    {
+        "name": "spin",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "islands"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "spin",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "s"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "form",
+        "elems": [
+            {
+                "name": "filter"
+            }
+        ]
+    },
+    {
+        "name": "checkbox"
+    },
+    {
+        "name": "checkbox",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "islands"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "checkbox",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "l"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "checkbox",
+        "mods": [
+            {
+                "name": "checked",
+                "vals": [
+                    {
+                        "name": true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "checkbox",
+        "mods": [
+            {
+                "name": "checked"
+            }
+        ]
+    },
+    {
+        "name": "sssr",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "island"
+    },
+    {
+        "name": "island",
+        "elems": [
+            {
+                "name": "header"
+            }
+        ]
+    },
+    {
+        "name": "user"
+    },
+    {
+        "name": "link"
+    },
+    {
+        "name": "user",
+        "elems": [
+            {
+                "name": "name"
+            }
+        ]
+    },
+    {
+        "name": "user",
+        "elems": [
+            {
+                "name": "post-time"
+            }
+        ]
+    },
+    {
+        "name": "image"
+    },
+    {
+        "name": "user",
+        "elems": [
+            {
+                "name": "icon"
+            }
+        ]
+    },
+    {
+        "name": "island",
+        "elems": [
+            {
+                "name": "text"
+            }
+        ]
+    },
+    {
+        "name": "island",
+        "elems": [
+            {
+                "name": "footer"
+            }
+        ]
+    },
+    {
+        "name": "service"
+    },
+    {
+        "name": "service",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "twitter"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "service",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "instagram"
+                    }
+                ]
+            }
+        ]
+    }
+];
---
+++ desktop.bundles/index/index.deps.js
@@ -0,0 +1,443 @@
+exports.deps = [
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "sssr"
+    },
+    {
+        "block": "sssr",
+        "elem": "header"
+    },
+    {
+        "block": "sssr",
+        "elem": "logo"
+    },
+    {
+        "block": "icon"
+    },
+    {
+        "block": "icon",
+        "mod": "type"
+    },
+    {
+        "block": "form"
+    },
+    {
+        "block": "form",
+        "elem": "search"
+    },
+    {
+        "block": "input"
+    },
+    {
+        "block": "input",
+        "mod": "0"
+    },
+    {
+        "block": "input",
+        "mod": "1"
+    },
+    {
+        "block": "input",
+        "elem": "box"
+    },
+    {
+        "block": "input",
+        "elem": "control"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "tick"
+    },
+    {
+        "block": "idle"
+    },
+    {
+        "block": "input",
+        "mod": "theme"
+    },
+    {
+        "block": "input",
+        "mod": "size"
+    },
+    {
+        "block": "input",
+        "mod": "has-clear"
+    },
+    {
+        "block": "input",
+        "elem": "clear"
+    },
+    {
+        "block": "button"
+    },
+    {
+        "block": "button",
+        "mod": "focused"
+    },
+    {
+        "block": "button",
+        "mod": "hovered"
+    },
+    {
+        "block": "button",
+        "mod": "disabled"
+    },
+    {
+        "block": "button",
+        "mod": "pressed"
+    },
+    {
+        "block": "button",
+        "elem": "text"
+    },
+    {
+        "block": "keyboard",
+        "elem": "codes"
+    },
+    {
+        "block": "button",
+        "mod": "theme"
+    },
+    {
+        "block": "button",
+        "mod": "size"
+    },
+    {
+        "block": "button",
+        "mod": "type"
+    },
+    {
+        "block": "spin"
+    },
+    {
+        "block": "spin",
+        "mod": "theme"
+    },
+    {
+        "block": "spin",
+        "mod": "size"
+    },
+    {
+        "block": "form",
+        "elem": "filter"
+    },
+    {
+        "block": "checkbox"
+    },
+    {
+        "block": "checkbox",
+        "mod": "disabled"
+    },
+    {
+        "block": "checkbox",
+        "mod": "focused"
+    },
+    {
+        "block": "checkbox",
+        "mod": "checked"
+    },
+    {
+        "block": "checkbox",
+        "elem": "box"
+    },
+    {
+        "block": "checkbox",
+        "elem": "control"
+    },
+    {
+        "block": "checkbox",
+        "mod": "theme"
+    },
+    {
+        "block": "button",
+        "mod": "togglable"
+    },
+    {
+        "block": "checkbox",
+        "mod": "size"
+    },
+    {
+        "block": "sssr",
+        "elem": "content"
+    },
+    {
+        "block": "island"
+    },
+    {
+        "block": "island",
+        "elem": "header"
+    },
+    {
+        "block": "user"
+    },
+    {
+        "block": "link"
+    },
+    {
+        "block": "link",
+        "mod": "disabled"
+    },
+    {
+        "block": "user",
+        "elem": "name"
+    },
+    {
+        "block": "user",
+        "elem": "post-time"
+    },
+    {
+        "block": "image"
+    },
+    {
+        "block": "user",
+        "elem": "icon"
+    },
+    {
+        "block": "island",
+        "elem": "text"
+    },
+    {
+        "block": "island",
+        "elem": "footer"
+    },
+    {
+        "block": "service"
+    },
+    {
+        "block": "service",
+        "mod": "type"
+    },
+    {
+        "block": "service",
+        "mod": "type",
+        "val": "instagram"
+    },
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "icon",
+        "mod": "type",
+        "val": "sssr"
+    },
+    {
+        "block": "input",
+        "mod": "0",
+        "val": "disabled"
+    },
+    {
+        "block": "input",
+        "mod": "1",
+        "val": "focused"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "input",
+        "mod": "theme",
+        "val": "islands"
+    },
+    {
+        "block": "input",
+        "mod": "size",
+        "val": "m"
+    },
+    {
+        "block": "input",
+        "mod": "has-clear",
+        "val": true
+    },
+    {
+        "block": "button",
+        "mod": "focused",
+        "val": "yes"
+    },
+    {
+        "block": "button",
+        "mod": "hovered",
+        "val": "yes"
+    },
+    {
+        "block": "button",
+        "mod": "disabled",
+        "val": "yes"
+    },
+    {
+        "block": "button",
+        "mod": "pressed",
+        "val": "yes"
+    },
+    {
+        "block": "button",
+        "mod": "theme",
+        "val": "islands"
+    },
+    {
+        "block": "button",
+        "mod": "size",
+        "val": "m"
+    },
+    {
+        "block": "button",
+        "mod": "type",
+        "val": "submit"
+    },
+    {
+        "block": "spin",
+        "mod": "theme",
+        "val": "islands"
+    },
+    {
+        "block": "spin",
+        "mod": "size",
+        "val": "s"
+    },
+    {
+        "block": "checkbox",
+        "mod": "disabled",
+        "val": true
+    },
+    {
+        "block": "checkbox",
+        "mod": "focused",
+        "val": true
+    },
+    {
+        "block": "checkbox",
+        "mod": "checked",
+        "val": true
+    },
+    {
+        "block": "button",
+        "mod": "togglable",
+        "val": "check"
+    },
+    {
+        "block": "checkbox",
+        "mod": "size",
+        "val": "l"
+    },
+    {
+        "block": "service",
+        "mod": "type",
+        "val": "twitter"
+    },
+    {
+        "block": "checkbox",
+        "mod": "theme",
+        "val": "islands"
+    }
+];
